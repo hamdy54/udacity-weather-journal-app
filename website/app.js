d41c846a442e129218834b4f0824fe8f7ccf7aaa
front-end147 <vigo_digo@mail.ru>
2021-12-23 08:13:38 +0200

    edit on code

--- website/app.js
+++ website/app.js
@@ -12,6 +12,13 @@ const apiKey = ",&appid=43fec62a8d6f0c1e4dd2c266d79261f0&units=metric";
 // server link to post data
 const server = "http://localhost:5500";
 
+// content variables
+let date = document.querySelector("#date");
+let temp = document.querySelector("#temp");
+let city = document.querySelector("#city");
+let desc = document.querySelector("#desc");
+let content = document.querySelector("#content");
+
 /* Functions */
 const generateData = () => {
   // value that the user types into the input and textarea
@@ -19,21 +26,15 @@ const generateData = () => {
     feeling = document.querySelector("#feelings").value;
 
   // weatherData return promise
-  weatherData(zip).then((data) => {
-    if (data) {
+  weatherData(zip).then((dt) => {
+    if (dt) {
       const {
         main: { temp },
         name: city,
+        sys: { country },
         weather: [{ description, icon }],
-      } = data;
-      const info = {
-        newDate,
-        temp: Math.round(temp),
-        city,
-        description,
-        feeling,
-        icon,
-      };
+      } = dt;
+      const info = [newDate, Math.round(temp), city, description, feeling, icon, country];
       postData(`${server}/add`, info);
       // calling updateUI function
       updateUI();
@@ -50,12 +51,12 @@ const weatherData = async (zip) => {
     }
     return data;
   } catch (error) {
-    console.log(`error: ${error}`);
+    alert(`error: ${error}`);
   }
 };
 
 // postData function
-const postData = async (url = "", info = {}) => {
+const postData = async (url = "", info = []) => {
   const res = await fetch(url, {
     method: "POST",
     // credentials: "same-origin",
@@ -70,23 +71,22 @@ const postData = async (url = "", info = {}) => {
     console.log("New Data: ", newData);
     return newData;
   } catch (error) {
-    console.log(`error: ${error}`);
+    alert(`error: ${error}`);
   }
 };
 
 // function to get project data and update UI using this data
 const updateUI = async () => {
-  // const res = await fetch(server + "/all");
   const res = await fetch(`${server}/all`);
   try {
     const finalData = await res.json();
-    document.querySelector("#date").innerHTML = finalData.newDate;
-    document.querySelector("#temp").innerHTML = `<img src="https://openweathermap.org/img/w/${finalData.icon}.png" alt"" /> ${finalData.temp}&deg;C`;
-    document.querySelector("#city").innerHTML = finalData.city;
-    document.querySelector("#desc").innerHTML = finalData.description;
-    document.querySelector("#content").innerHTML = finalData.feeling;
+    date.innerHTML = finalData[0];
+    temp.innerHTML = `<img src="https://openweathermap.org/img/w/${finalData[5]}.png" alt"" /> ${finalData[1]}&deg;C`;
+    city.innerHTML = `${finalData[2]}, ${finalData[6]}`;
+    desc.innerHTML = finalData[3];
+    content.innerHTML = finalData[4];
   } catch (error) {
-    console.log(`error: ${error}`);
+    alert(`error: ${error}`);
   }
 };
 
